1abf4515e8cea421e0d77e0a0e348e9ccb363b3b
yann1294 <dev.yann12@gmail.com>
2020-04-17 10:05:12 +0530

    Initial Commit

--- demo/src/main/java/demo/DemoApplication.java
+++ demo/src/main/java/demo/DemoApplication.java
@@ -9,7 +9,11 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
-        HelloWorld helloWorld = (HelloWorld) ctx.getBean("helloWorld");
-        helloWorld.sayHello();
+        InjectedByConstructorService constructorService = (InjectedByConstructorService) ctx.getBean("injectedByConstructorService");
+        constructorService.getMessage();
+        SetterBasedService setterBasedService = (SetterBasedService) ctx.getBean("setterBasedService");
+        setterBasedService.getMessage();
+        HelloWorldImpl helloWorldImpl = (HelloWorldImpl) ctx.getBean("helloWorldImpl");
+        helloWorldImpl.sayHello();
     }
 }
--- demo/src/main/java/demo/HelloWorldImpl.java
+++ demo/src/main/java/demo/HelloWorldImpl.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Component;
  * Created by jt on 3/11/15.
  */
 @Component
-public class HelloWorld {
+public class HelloWorldImpl implements HelloWorldService{
+
     public void sayHello(){
+
         System.out.println("Hello World!!!!");
     }
 }
